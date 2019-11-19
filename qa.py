ce2e537720d7a725eff2d9a352359780638e5c37
Atticus Deutsch <afd998@gmail.com>
2019-11-19 11:55:02 -0800

    Initial commit

---
+++ qa.py
@@ -0,0 +1,238 @@
+from qa_engine.base import QABase
+import spacy
+import nltk
+from nltk.corpus import stopwords
+import copy
+import pickle
+
+nlp = spacy.load("en_core_web_lg")
+#test
+questions = 0
+
+stop = set(stopwords.words('english'))
+
+
+def token_filter(token, doc, take_out_capitals=False, use_spacy_stopwords=False):
+    keep_token = True
+    if token.pos_ == "PROPN":
+        keep_token = False
+    elif take_out_capitals and (token.text is not doc[0].text) and (token.text is not token.text.lower()):
+        keep_token == False
+    if token.text == "-PRON-":
+        keep_token = False
+    elif token.text.lower(
+    ) in stop if not use_spacy_stopwords else token.is_stop:
+        keep_token = False
+
+    elif token.is_punct:
+        keep_token = False
+    return keep_token
+
+
+def expand_synsets(token):
+    out_set = set()
+    pos_dict = {
+        "NOUN": wn.NOUN,
+        "VERB": wn.VERB,
+        "ADV": wn.ADV,
+        "ADJ": wn.ADJ
+    }
+    if token.pos_ in pos_dict:
+        synset = wn.synsets(token.lemma_, pos=pos_dict[token.pos_])
+    else:
+        synset = wn.synsets(token.lemma_)
+
+    if synset != []:
+        for synonym in synset[0].lemma_names():
+            out_set.add(synonym)
+            for syn in wn.synsets(synonym)[0].lemma_names():
+                out_set.add(syn)
+                if len(wn.synsets(synonym)) > 1:
+                    for syn2 in wn.synsets(synonym)[1].lemma_names():
+                        out_set.add(synonym)
+        if len(synset) > 1:
+            for synonym in synset[1].lemma_names():
+                out_set.add(synonym)
+    else:
+        out_set.add(token.lemma_)
+    return out_set
+
+
+def normalize(text, output_type="set", take_out_caps=False, lemmatize=True, expand_synsets=True, loose_filter=False):
+    if expand_synsets and not lemmatize:
+        print("can't expand synsets without lemmatizing", file=sys.stderr)
+        exit()
+
+    doc = nlp(text)
+    for token in doc:
+        if token_filter(token, doc, take_out_capitals=take_out_caps):
+            if expand_synsets:
+                expand_synsets(token)
+            if expand_synsets:
+                pos_dict = {
+                    "NOUN": wn.NOUN,
+                    "VERB": wn.VERB,
+                    "ADV": wn.ADV,
+                    "ADJ": wn.ADJ
+                }
+                if token.pos_ in pos_dict:
+                    synset = wn.synsets(token.lemma_, pos=pos_dict[token.pos_])
+                else:
+                    synset = wn.synsets(token.lemma_)
+
+                if synset != []:
+                    for synonym in synset[0].lemma_names():
+                        out_set.add(synonym)
+                        for syn in wn.synsets(synonym)[0].lemma_names():
+                            out_set.add(syn)
+                            if len(wn.synsets(synonym)) > 1:
+                                for syn2 in wn.synsets(synonym)[1].lemma_names():
+                                    out_set.add(synonym)
+                    if len(synset) > 1:
+                        for synonym in synset[1].lemma_names():
+                            out_set.add(synonym)
+                else:
+                    out_set.add(token.lemma_)
+            elif (lemmatize):
+                out_set.add((token.lemma_
+                             if token.lemma_ != "-PRON-" else token.text) + " ")
+            else:
+                out_set.add(token.text + " ")
+    if output_type == "string":
+        list1 = list(out_set)
+        out = ''.join(list1)
+    else:
+        out = out_set
+    return nlp(out)
+
+
+def coreferece_story(story, load):
+    ### coreference is incomplete due to cofreference overlap give in JSON ###
+    if not load:
+        print("question")
+        sent = story[0]
+        story_coref = sent['coref']
+        localstory = copy.deepcopy(story)
+        for i in range(0, len(story)):
+            # print(story[i]["sentence"])
+            localstory[i]["sentence"] = nltk.word_tokenize(story[i]["sentence"])
+            # print(localstory[i]["sentence"])
+
+        for key in story_coref:
+            print("new anticident")
+            anticident = story_coref[key][0]["text"]
+            for z in range(1, len(story_coref[key])):
+                sentind = story_coref[key][z]["sentNum"]
+                example_text = story_coref[key][z]["text"]
+                anticident_tokens = nltk.word_tokenize(anticident)
+                print("anticident_tokens:", anticident_tokens)
+                example_tokens = nltk.word_tokenize(example_text)
+                print("example_tokens:", example_tokens)
+                if example_tokens[0] in localstory[sentind - 1]["sentence"]:
+                    example_start = localstory[sentind - 1]["sentence"].index(example_tokens[0])
+                    if localstory[sentind - 1]["sentence"][example_start - 1] != 'was':
+                        if localstory[sentind - 1]["sentence"][example_start - 1] != 'is':
+                            if localstory[sentind - 1]["sentence"][example_start - 2] != anticident_tokens[
+                                len(anticident_tokens) - 1]:
+                                print("Before:", localstory[sentind - 1]["sentence"])
+                                for i in range(0, len(example_tokens)):
+                                    del localstory[sentind - 1]["sentence"][example_start]
+                                print("During:", localstory[sentind - 1]["sentence"])
+                                for i in range(0, len(anticident_tokens)):
+                                    localstory[sentind - 1]["sentence"].insert(example_start + i, anticident_tokens[i])
+                                print("After:", localstory[sentind - 1]["sentence"])
+        coref_file = open(
+            "data/coref_sents-{0}.pickle".format(story[0]["storyid"]), 'wb')
+        pickle.dump(localstory, coref_file)
+        coref_file.close()
+        return localstory
+    else:
+        coref_file = open(
+            "data/coref_sents-{0}.pickle".format(story[0]["storyid"]), 'rb')
+        localstory = pickle.load(coref_file)
+        coref_file.close()
+        return localstory
+
+
+'''
+def person_in_the_question(question):
+  ner_list=question[ner]
+  for entry in ner_list:
+    if (entry[ner] == 'PERSON') or (entry[ner] == 'TITLE'):
+      if(entry[text]!= ("he" or "He" or "she" or "She" or "his" or "hers" or "His" or"Hers"):
+        output = entry[text]
+        break
+  return output
+'''
+'''def narrow_sentences_by_Who_Where(coref_story, question)
+  if nltk.word_tokenize(question["question"])[0]== "Where":
+    senetence_list= []
+  for sentnce in coref_story:
+ '''
+
+
+def get_answer(question, story):
+    """
+    :param question: dict
+    :param story: dict
+    :return: answerid, answer
+
+
+    question is a dictionary with keys:
+        question -- The raw text of question.
+        storyid --  The story id.
+        questionid  --  The id of the question.
+        tokens -- Stanford CoreNLP version of word tokenization with POS
+        ner -- Stanford CoreNLP version of NER
+
+
+    story is a list of sentence, each sentence is a dictionary with keys:
+        storytitle -- the title of the story.
+        storyid --  the id of the story.
+        sentence -- the raw text sentence version.
+        sentenceid --  the id of the sentence
+        tokens -- Stanford CoreNLP version of word tokenization with POS
+        ner -- Stanford CoreNLP version of NER
+        coref -- Stanford CoreNLP version of coreference resolution of the entire story
+
+    """
+
+    ###     Your Code Goes Here         ###
+    print("NEW QUESTION")
+    load = False
+    coref_story = coreferece_story(story, load)
+    # person_in_the_question= person_in_the_question(question)
+    # sentences = narrow_sentences_by_Who(coref_story, question)
+
+    answer = "whatever you think the answer is"
+    answerid = "-"
+
+    ###     End of Your Code         ###
+    return answerid, answer
+
+
+#############################################################
+###     Dont change the code in this section
+#############################################################
+class QAEngine(QABase):
+    @staticmethod
+    def answer_question(question, story):
+        answerid, answer = get_answer(question, story)
+        return (answerid, answer)
+
+
+def run_qa():
+    QA = QAEngine()
+    QA.run()
+    QA.save_answers()
+
+
+#############################################################
+
+
+def main():
+    run_qa()
+
+
+if __name__ == "__main__":
+    main()
---
+++ qa_engine/base.py
@@ -0,0 +1,101 @@
+import json, ast
+import pandas as pd
+from nltk.parse import DependencyGraph
+from nltk.tree import Tree
+from collections import defaultdict
+
+# Questionid may also be referred to as qid in variable names
+# Storyid may also be referred to as sid in variable names
+
+DATA_DIR = "data/"
+QUESTION_FILE = "hw7-questions.tsv"
+ANSWER_FILE = "hw7-answers.tsv"
+STORIES_FILE = "hw7-stories.tsv"
+COREF_FILE = "hw7-stories-coref.tsv"
+
+RESPONSE_FILE = "hw7-responses.tsv"
+
+
+# def prepare_deps(raw_deps):
+#
+#    if isinstance(raw_deps, float) and math.isnan(raw_deps):
+#        return []
+#    return [DependencyGraph(dep, top_relation_label="root") for dep in raw_deps.split("\n\n")
+#            if len(dep) > 2]
+#
+#
+# # TO BE USED IN HW 7 & 8
+# def prepare_pars(raw_pars):
+#    if isinstance(raw_pars, float) and math.isnan(raw_pars):
+#        return []
+#
+#    return [Tree.fromstring(line.strip().rstrip(" \n\t"))
+#            for line in raw_pars.split("\n") if len(line) > 2]
+
+def prepare_story_data(df_story, df_coref):
+    stories = defaultdict(list)
+    for row in df_story.itertuples():
+        this_story = {
+            "storytitle": row.storytitle,
+            "sentence": row.sentence,
+            "sentenceid": row.sentenceid,
+            "storyid": row.storyid,
+            "ner": ast.literal_eval(row.ner),
+            "coref": json.loads(df_coref.loc[row.storyid, 'coref'])
+        }
+        stories[row.storyid] += [this_story]
+    return stories
+
+
+def prepare_questions(df):
+    questions = {}
+    for row in df.itertuples():
+        this_qstn = {
+            "questionid": row.questionid,
+            "storyid": row.storyid,
+            "question": row.question,
+            "ner": ast.literal_eval(row.ner)
+        }
+        questions[row.questionid] = this_qstn
+    return questions
+
+
+class QABase(object):
+
+    def __init__(self):
+
+        self._stories = prepare_story_data(pd.read_csv(DATA_DIR + STORIES_FILE, sep="\t"),
+                                           pd.read_csv(DATA_DIR + COREF_FILE, sep="\t", index_col="storyid"))
+        self._questions = prepare_questions(pd.read_csv(DATA_DIR + QUESTION_FILE, sep="\t"))
+        self._answers = {q["questionid"]: "" for q in self._questions.values()}
+
+
+    @staticmethod
+    def answer_question(question, story):
+        # input: question and story
+        # output: sentenceid and answer text
+        # HW6 only: answer text can be an empty string ""
+        raise NotImplemented
+
+
+    def get_question(self, qid):
+        return self._questions.get(qid)
+
+
+    def get_story(self, sid):
+        return self._stories.get(sid)
+
+
+    def run(self):
+        for qid, q in self._questions.items():
+            output = self.answer_question(q, self.get_story(q["storyid"]))
+            self._answers[qid] = {"storyid": q["storyid"], "questionid": qid, \
+                                    "answer_sentenceid": output[0],"answer": output[1],}
+
+
+    def save_answers(self, fname=RESPONSE_FILE):
+        df = pd.DataFrame([a for a in self._answers.values()])
+        df.to_csv(fname, sep="\t", index=False)
+
+
+
